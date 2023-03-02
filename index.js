bbba8324f1e9e7f1733d61b8593a902aab616992
Diego Silva <odiegosilvadev@gmail.com>
2023-03-02 19:04:04 -0300

    Update index.js
    add a new class

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
-const Cliente1Nome = "Diego";
-const Cliente1CPF = "12366677701";
-const Cliente1Agencia = 1001;
-const Cliente1Saldo = 0; 
+
+//criando aprimeira classe e atributos
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new Cliente();
+
+        Cliente1.nome = "Diego";
+        Cliente1.cpf = "12366677701";
+        Cliente1.agencia = 1001;
+        Cliente1.saldo = 0; 
 
 const Cliente2Nome = "Letícia";
 const Cliente2CPF = "33312346702";
 const Cliente2Agencia = 1001;
 const Cliente2Saldo = 0; 
-
