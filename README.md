3f9def96b838c2e62e6ee9ac4ea5df78b42956bc
Diego Silva <odiegosilvadev@gmail.com>
2023-03-02 10:56:03 -0300

    create index.js
    add variables

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Bytebank
-Sistema de conta-corrente e cadastro de clientes usando javaScript.
+Sistema de conta-corrente e cadastro de clientes usando javaScript POO.
+
+** manipular o saldo desses usuários e realizar operações bancárias.
+
+
---
+++ index.js
@@ -0,0 +1,10 @@
+const Cliente1Nome = "Diego";
+const Cliente1CPF = "12366677701";
+const Cliente1Agencia = 1001;
+const Cliente1Saldo = 0; 
+
+const Cliente2Nome = "Letícia";
+const Cliente2CPF = "33312346702";
+const Cliente2Agencia = 1001;
+const Cliente2Saldo = 0; 
+
